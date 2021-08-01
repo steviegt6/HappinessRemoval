e96da28ef0d0f9b8671530cb0b0dbfcdf09f162b
Stevie <xxlennygamerxx@gmail.com>
2021-08-01 02:30:54 -0700

    Update to the latest tModLoader alpha and replace convoluted IL editing with a simple button removal.

--- HappinessRemoval.cs
+++ HappinessRemoval.cs
@@ -1,8 +1,6 @@
-using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System.ComponentModel;
-using System.Reflection;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
@@ -14,79 +12,20 @@ namespace HappinessRemoval
         public override void Load()
         {
             IL.Terraria.Main.DrawNPCChatButtons += Main_DrawNPCChatButtons;
-            IL.Terraria.Main.GUIChatDrawInner += Main_GUIChatDrawInner;
         }
 
         public override void Unload()
         {
             IL.Terraria.Main.DrawNPCChatButtons -= Main_DrawNPCChatButtons;
-            IL.Terraria.Main.GUIChatDrawInner -= Main_GUIChatDrawInner;
         }
 
-        private void Main_DrawNPCChatButtons(ILContext il)
+        private static void Main_DrawNPCChatButtons(ILContext il)
         {
-            FieldInfo happinessText = typeof(HappinessRemoval).GetField("HappinessText", BindingFlags.Public | BindingFlags.Static);
-            MethodInfo get_Black = typeof(Color).GetMethod("get_Black", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
-            ILCursor c = new ILCursor(il);
+            ILCursor c = new(il);
 
-            if (!c.TryGotoNext(i => i.MatchLdstr("UI.NPCCheckHappiness")))
-            {
-                Logger.Error("UI.NPCCheckHappiness");
-                return;
-            }
-
-            if (!c.TryGotoNext(i => i.MatchLdloca(9)))
-            {
-                Logger.Error("Ldloca 9");
-                return;
-            }
-
-            c.Index++;
-
-            c.RemoveRange(9);
-
-            c.Emit(OpCodes.Ldc_I4, 100);
-            c.Emit(OpCodes.Ldc_I4, 100);
-            c.Emit(OpCodes.Ldc_I4, 100);
-
-            if (!c.TryGotoNext(i => i.MatchLdloc(11)))
-            {
-                Logger.Error("Ldloc 11");
-                return;
-            }
-
-            if (!c.TryGotoNext(i => i.MatchLdloc(15)))
-            {
-                Logger.Error("Ldloc 15");
-                return;
-            }
-
-            c.Index++;
-
+            c.GotoNext(MoveType.After, x => x.MatchLdstr("UI.NPCCheckHappiness"));
             c.Emit(OpCodes.Pop);
-
-            c.Emit(OpCodes.Call, get_Black);
-        }
-
-        private void Main_GUIChatDrawInner(ILContext il)
-        {
-            ILCursor c = new ILCursor(il);
-
-            if (!c.TryGotoNext(i => i.MatchLdsfld("Terraria.Main", "npcChatFocus4")))
-            {
-                Logger.Error("Terraria.Main npcChatFocus4");
-                return;
-            }
-
-            if (!c.TryGotoNext(i => i.MatchPop()))
-            {
-                Logger.Error("Pop");
-                return;
-            }
-
-            c.Index++;
-
-            c.RemoveRange(6);
+            c.Emit(OpCodes.Ldstr, string.Empty);
         }
     }
 
@@ -100,7 +39,7 @@ namespace HappinessRemoval
         [Slider]
         [DefaultValue(0.75f)]
         [Range(0.5f, 2f)]
-        public float npcHappiness;
+        public float NpcHappiness;
 
         public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message)
         {
@@ -117,13 +56,13 @@ namespace HappinessRemoval
     {
         public override void PreUpdate()
         {
-            Player.currentShoppingSettings.PriceAdjustment = ModContent.GetInstance<HappinessConfig>().npcHappiness;
+            Player.currentShoppingSettings.PriceAdjustment = ModContent.GetInstance<HappinessConfig>().NpcHappiness;
 
-            if (Main.npcChatFocus4)
-            {
-                Main.instance.MouseText("NPC happiness disabled by NPC Happiness Removal!");
-                Main.mouseText = true;
-            }
+            if (!Main.npcChatFocus4)
+                return;
+
+            Main.instance.MouseText("NPC happiness disabled by NPC Happiness Removal!");
+            Main.mouseText = true;
         }
     }
 }
